a88fba9a454397a76975e4ba49e273b34d34bf55
jose.115 <joseveiga1234@gmail.com>
2022-09-26 12:36:13 -0300

    Primeiro commit

---
+++ src/view/Search/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CardSearch from '../../Components/CardSearch';
+
+// import { Container } from './styles';
+
+const Search: React.FC = () => {
+  return (<div className="app_wrapper">
+    <header>
+      <ul className='menu'>
+        <li><Link to="/">Listar</Link></li>
+        <li><Link to="/buscar">Buscar</Link></li>
+        <li><Link to="/sobre">Sobre</Link></li>
+      </ul>
+    </header>
+    <h1>Buscando contatos</h1>
+    <CardSearch />
+  </div>);
+
+}
+
+export default Search;
